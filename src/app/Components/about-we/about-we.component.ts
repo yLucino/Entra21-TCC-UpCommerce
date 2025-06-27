89aa8206bb61b9cf7d6277a0ad915193383c0799
Vitor Moraes <vitordemoraesfilho@gmail.com>
2025-06-27 09:34:13 -0300

    about

---
+++ src/app/Components/about-we/about-we.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-we',
+  templateUrl: './about-we.component.html',
+  styleUrls: ['./about-we.component.css']
+})
+export class AboutWeComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
+import { AboutWeComponent } from './components/about-we/about-we.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutWeComponent
   ],
   imports: [
     BrowserModule,
